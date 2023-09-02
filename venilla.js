b21525cfaccbfd9fcc3cb5f30e96f6163d6f911f
tjay1505 <119523945+tjay1505@users.noreply.github.com>
2023-09-02 21:42:44 +0530

    9:42

--- venilla.js
+++ venilla.js
@@ -1,5 +1,6 @@
 const root = document.querySelector(':root')
 const menu = document.getElementById('menu')
+const header = document.querySelector('.header')
 let Theme = true 
 
 
@@ -13,6 +14,14 @@ function invertColor(){
     }
 
 }
+function headerbg(){
+    if(Theme){
+        header.classList.add('header-adder')
+    }
+    else{
+        header.classList.remove('header-adder')
+    }
+}
 function changeTheme(){
     let rs = getComputedStyle(root)
    
@@ -20,19 +29,33 @@ function changeTheme(){
         root.style.setProperty('--black', 'white');
         root.style.setProperty('--white', 'black');
         console.log(rs.getPropertyValue('--bgcblack'));
-        //root.style.setProperty('--bgcblack',rgb(236, 236, 236));
-
+        headerbg()
         invertColor()
 
     }
+
     else{
         root.style.setProperty('--black', 'black');
         root.style.setProperty('--white', 'white');
-       // root.style.setProperty('--bgcblack','rgb(236, 236, 236)' );
-
+        headerbg()
         invertColor()
     }
     Theme = !Theme  
 }
 
 document.body.style.filter = 'blur(9)'
+
+const images = ["./assets/img_1.jpg","./assets/KaSVZO2q_400x400.jpg","./assets/bootstrap-social.png","./assets/fr.jpg","./assets/github-cover.jpg",
+'./assets/img-javascript.webp',"./assets/img-sass.png","./assets/Best-JavaScript-Frameworks-to-use-in-2020.jpg","./assets/vscode.jpg"
+]
+
+const mainimg = document.getElementById('main-img')
+let adder = 0
+mainimg.setAttribute('src',images[adder])
+
+function imgChanger(){
+    adder++
+    mainimg.setAttribute('src',images[adder])
+}
+
+setInterval(() => {imgChanger()},3000)
